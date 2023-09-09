b605ed129e4647a38d874056cadf50a797aa569f
AdeoluwaO <odejobipaul10@gmail.com>
2023-09-09 12:53:52 +0100

    save folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppPerformance());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppPerformance extends StatelessWidget {
+  const AppPerformance({super.key});
 
   // This widget is the root of your application.
   @override
